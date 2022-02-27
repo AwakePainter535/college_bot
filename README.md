a7ca5feb075a5c700380fe4ff8d97398d7ae8391
oleary11 <jasonmendoza12003@gmail.com>
2022-02-27 14:52:35 -0700

    updated read me and iterator

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # college_bot
-Step 1: Follow instructions on TikTok to download python and pyautogui, links can be found here:
+DISCLAIMER - Try this on your own accord, I'm not responsible for anything that happens. If you are using GMail, it limits you to 500 email per day, this is why the counter iterates 500 times on the python bot. I made 4 emails and just made the iterator go from 0-499, 500 - 999, 1000 - 1499, 1500 - 1716 i.e. update the x and i values respectively). After each set of 500 emails, log out and log into the next email you're using. If you need to stop the program once it's started, move your cursor to the upper-left corner of the leftmost monitor, it's a built in failsafe for pyautogui. It takes quite a while for the bot to write out and send every email.
+
+Follow these steps:
+• Step 1: Follow instructions on TikTok to download python and pyautogui, links can be found here:
 	• https://www.python.org/downloads/
 	• https://pyautogui.readthedocs.io/en/latest/install.html
 	
-Step 2: Download .CSV and .py files from GitHub (save all in one file location)
+• Step 2: Download .CSV and .py files from GitHub (save all in one file location)
+
+• Step 3: Press the Windows key, type IDLE and press enter. Open position.py and collegeemailer.py (File > Open > (location of collegeemailer.py and position.py) and open both .py files)
+
+• Step 4: Run position.py (Run > Run Module > move mouse to each button) to get the (x,y) coordinates for each button (compose, subject line, email body, and send all are labeled in comments) the format is (x = x_coord, y = y_coord) (for example (x = 1230, y = 540)) 
+
+• Step 5: Change the following values to your information respectively: ENTER_NAME (x2), ENTER_HIGHSCHOOL, ENTER_STATE, ENTER_ADDRESS. (If you don't add your address in the email you're much less likely to get stuff from the colleges.)
+
+• Step 6: Run college_emailer.py (Run > Run Module) click into the window gmail is on and sit back and watch!
 	
--- collegeemailer.py
+++ collegeemailer.py
@@ -10,24 +10,24 @@ with open('collegeemails.csv', newline='') as csvfile:
 with open('collegenames.csv', newline='') as csvfile:
     names = list(csv.reader(csvfile))
     
-x = 424
-i = 924
-while i >= x:
+x = 0 #startine
+i = 499 #endline
+while x <= i:
     time.sleep(1)
     pyautogui.click(x,y values from position.py) #click on compose
     time.sleep(0.3)
-    pyautogui.write(emails[i][0]) #write name into receipient field
+    pyautogui.write(emails[x][0]) #write name into receipient field
     time.sleep(0.3)
-    pyautogui.click(x,y values from position.py) #click on subject
+    pyautogui.click(x,y values from position.py) #click on subject line
     time.sleep(0.3)
     pyautogui.write("Attending ") #Writing on subject line
-    pyautogui.write(names[i][0]) #Writing on subject line
+    pyautogui.write(names[x][0]) #Writing on subject line
     pyautogui.write(" in the Fall") #Writing on subject line
     time.sleep(0.3)
     pyautogui.click(x,y values from position.py) #click on email body
     time.sleep(0.3)
     pyautogui.write("Hi there, my name is ENTER_NAME and I am currently a senior at ENTER_HIGHSCHOOL in ENTER_STATE. I am planning on attending ") #Writing on email body
-    pyautogui.write(names[i][0]) #Writing on email body
+    pyautogui.write(names[x][0]) #Writing on email body
     pyautogui.write(" in the fall of 2022. I toured your campus and it just feels like the perfect fit for me. I'm sure you get a lot of these requests, but I was wondering if there is any chance I could receive a pennant, flag, shirt or really anything you guys are able to offer me! I would love to start representing my school pride as early as possible. If you guys are able to help me out, my address is ENTER_ADDRESS I am looking forward to being a student and getting my collegiate career started!") #Writing on email body
     time.sleep(0.3)
     pyautogui.press('enter') #paragraph break
@@ -36,4 +36,4 @@ while i >= x:
     time.sleep(0.3)
     pyautogui.write("All the best, ENTER_NAME.") #Writing on email body
     pyautogui.click(x,y values from position.py)#clicking send
-    i = i - 1
+    x = x + 1
